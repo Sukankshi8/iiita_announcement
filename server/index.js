a416f121063044e3c71980dc1248614fa4adbf4e
Sukankshi8 <sukankshi08@gmail.com>
2023-07-01 00:28:52 +0530

    new 10

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 //components
-import Connection from './database/db.js';
-import Router from './routes/route.js';
-
+import Connection from "./database/db.js";
+import Router from "./routes/route.js";
 
 dotenv.config();
 
@@ -14,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["https://iiita-announcement-frontend.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
@@ -22,12 +21,11 @@ app.use(
 
 app.use(express.json());
 app.get("/", (req, res) => {
-    res.send("API is running");
+  res.send("API is running");
 });
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', Router);
-
+app.use("/", Router);
 
 const PORT = 8000;
 const username = process.env.DB_USERNAME;
@@ -35,4 +33,6 @@ const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server is running successfully on PORT ${PORT}`)
+);
